119992063280b68c350c74de64307ceeb8cedca7
Ayush Jain <116271677+AyushJainA@users.noreply.github.com>
2023-01-06 16:03:55 +0530

    Create Basics.cpp

---
+++ Recursion/Basics.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+	int sum(int n ,int ans =0,int i=1){
+		if(i>n){return ans;	}
+		ans=ans+i;
+		i++;
+		sum(n, ans ,i);
+		
+	}
+	int f(int i,int sum=0){
+		cout<<"Backtracking++"<<endl;
+		if(i<1){cout<<sum;return 0;
+		}
+		sum=sum+i;
+		f(i-1,sum);
+		cout<<endl<<"Backtracking";
+	}
+	
+	//functional recurssion --> which returns somthing
+	
+	int fnc(int i ){
+		if(i==0) { return 0;
+		}
+		return i+fnc(i-1);
+	}
+	
+	int factorila(int n){
+		if(n==1){return 1;
+		}
+		return n*factorila(n-1);
+	
+	}
+	
+int main(){
+	int n;
+	cin>>n;
+	cout<<sum(n)<<endl;
+	f(n);
+	
+	cout<<endl<<"Factorial of n is "<<factorila(n)<<endl;
+return 0;
+}
+
