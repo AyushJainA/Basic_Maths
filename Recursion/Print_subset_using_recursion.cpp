dc9603c07149e0f4b383dca3e201498248d0147d
Ayush Jain <116271677+AyushJainA@users.noreply.github.com>
2023-01-08 19:42:45 +0530

    Add files via upload

---
+++ Recursion/Print_subset_using_recursion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void pnt(vector<int> arr, vector<int> &ds ,int n,int index =0){
+    // base condition 
+    if(index == n){
+        for(auto it : ds){cout<< it <<" ";}
+        if(ds.size()==0){cout<<"{}";}
+        cout<<endl;
+        return ;
+    }
+
+//take the first index 
+ds.push_back(arr[index]);
+pnt(arr,ds,n,index+1);
+
+//not take the first index-->now we will pop out the taken index
+ds.pop_back();
+pnt(arr,ds,n,index+1);
+
+}
+
+int main(){
+
+vector<int> arr = { 3,1,2};
+int n = arr.size();
+vector<int> ds;// ye empty vector hai jo print hoga 
+
+pnt(arr,ds,n);
+
+    return 0;
+}
