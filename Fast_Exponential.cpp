28bef2352313eefeb1f8f6973265ee2da8221a54
Ayush Jain <116271677+AyushJainA@users.noreply.github.com>
2023-01-27 14:21:39 +0530

    Update and rename Fast_Exponential to Fast_Exponential.cpp

--- Fast_Exponential.cpp
+++ Fast_Exponential.cpp
@@ -4,7 +4,7 @@ int modularExponentiation(int x, int n, int m) {
 	
     int res=1;
     while(n>0)   {
-        if(n&1){ //checking odd or even 
+        if(n&1){ //checking odd 
     res= (1LL * (res) *(x) )%m ; }  // 1LL range me lane k liye 
         x=(1LL * (x)%m *(x)%m);
         n=n>>1; //divide by 2 
